4f02010ff309db7df44075d6c61f35868a73b216
Nguyen Minh Tuan <tnowad@gmail.com>
2024-12-20 02:41:10 +0700

    feat: add config plugin

--- lua/lazylite/plugins/init.lua
+++ lua/lazylite/plugins/init.lua
@@ -1,1 +1,6 @@
 require("lazylite.config").init()
+
+return {
+  { "folke/lazy.nvim", version = "*" },
+  { "tnowad/lazylite", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
+}
